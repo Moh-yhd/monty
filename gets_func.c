12bffa29c9ea8170e0e4ff6e17393476f4eb4cc3
Mohammmed <mohammedthe11th@gmail.com>
2023-01-20 21:38:14 +0300

    edge cases

--- gets_func.c
+++ gets_func.c
@@ -12,7 +12,7 @@
 void get_instruction(stack_t **stack, char *token, unsigned int line_number)
 {
 	instruction_t table[] = {{"pop", pop}, {"pall", pall}, {"pint", pint}};
-	char *token_copy = NULL,  *copy2 = NULL;
+	char *token_copy = NULL,  *copy2 = NULL, *token2 = NULL;
 	size_t i;
 
 	if (token == NULL || *token == '\0')
@@ -20,15 +20,16 @@ void get_instruction(stack_t **stack, char *token, unsigned int line_number)
 
 	token_copy = malloc(sizeof(char) * (strlen(token) + 1));
 	strcpy(token_copy, token);
+	token2 = strtok(token_copy, " ");
 	if (token_copy == NULL)
 		return;
-	if (strncmp(token_copy, "push", 4) == 0)
+	if (strncmp(token2, "push", 4) == 0)
 	{
-		push(stack, token_copy, line_number);
+		push(stack, token, line_number);
 		free(token_copy);
 		return;
 	}
-	copy2 = strtok(token_copy, "$");
+	copy2 = strtok(token, "$");
 	if (copy2 == NULL)
 	{
 		free(token_copy);
@@ -36,7 +37,7 @@ void get_instruction(stack_t **stack, char *token, unsigned int line_number)
 	}
 	for (i = 0; i < (sizeof(table) / sizeof(instruction_t)); i++)
 	{
-	if (strcmp(token_copy, table[i].opcode) == 0
+	if (strcmp(token2, table[i].opcode) == 0
 			|| strcmp(copy2, table[i].opcode) == 0)
 	{
 		table[i].f(stack, line_number);
@@ -44,6 +45,33 @@ void get_instruction(stack_t **stack, char *token, unsigned int line_number)
 		return;
 	}
 	}
+	if (is_line_space(copy2) == 1)
+		return;
 	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, token);
 	exit(EXIT_FAILURE);
+}
+
+/**
+ * is_line_space - checks if a line is only space
+ * @line: is a pointer to a line
+ *
+ * Return: 1 if line is only space 0 if not
+ */
+int is_line_space(char *line)
+{
+	char *token = NULL;
+	size_t i;
+	int flag = 0;
+
+	token = strtok(line, "$");
+	for (i = 0; i < strlen(token); i++)
+	{
+		if (token[i] != ' ' && token[i] != '\n')
+			flag = 1;
+	}
+	if (flag == 0)
+		return (1);
+	else
+		return (0);
+
 }
