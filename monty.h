9a435846acd079bc94992d7d1cd9eec8f6343efa
Mohammmed <mohammedthe11th@gmail.com>
2023-01-20 16:45:16 +0300

    betty

--- monty.h
+++ monty.h
@@ -1,3 +1,6 @@
+#ifndef MONTY_H
+#define MONTY_H
+
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -15,9 +18,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -30,8 +33,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 void push(stack_t **stack, char *token, unsigned int line_number);
@@ -39,3 +42,5 @@ void pop(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void get_instruction(stack_t **stack, char *token, unsigned int line_number);
 int is_line_space(char *line);
+
+#endif /* #ifndef MONTY_H */
